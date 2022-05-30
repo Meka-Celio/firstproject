358ee97b3e6c6d664cfe7da14d5ec2b0447ca825
MEKA Célestin <45108048+Meka-Celio@users.noreply.github.com>
2022-05-30 17:58:51 +0100

    first commit + add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
+import { AppareilComponent } from './appareil/appareil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    AppareilComponent
   ],
   imports: [
     BrowserModule
